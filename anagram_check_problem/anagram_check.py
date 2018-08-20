b06c34b499c55b0d6bdc997a0eacf1bb8c215b14
Michael Lenghel <michael.lenghel@gmail.com>
2018-08-20 15:15:28 +0100

    Fixed bug and got rid of irrelevant code

--- anagram_check_problem/anagram_check.py
+++ anagram_check_problem/anagram_check.py
@@ -1,12 +1,10 @@
 # Program that will check if two strings are anagrams, not including captials or spaces
 
-from collections import defaultdict
-
 # Has a time complexity of O(N), ideal for small data sets, but space complexity is of O(26). As 26 references are made from hashmap.
 def anagram_check(s1, s2):
 
-	# Sets the default value of all keys to zero
-	ana_li = defaultdict(default_value)
+	# Declare the dictionary
+	ana_li = {}
 	# Removes spaces in both strings
 	s1 = s1.replace(" ", "").lower()
 	s2 = s2.replace(" ", "").lower()
@@ -17,18 +15,21 @@ def anagram_check(s1, s2):
 
 	# Increment by one for each dictionary letter key every time a letter is incurred
 	for letter in s1:
-		ana_li[letter] += 1
+		if letter in ana_li:
+			ana_li[letter] += 1
+		else:
+			ana_li[letter] = 1
 
 	# Decrement respective keys incremented previously, return False if we have past zero.
 	for letter in s2:
-		if ana_li[letter] < 0:
-			return False
-		else:
+		if letter in s2:
 			ana_li[letter] -= 1
+		else:
+			ana_li[letter] = 1
 
 	# If all values have not eached 0, return False, as imbalance of letters
-	for letter in ana_li:
-		if ana_li[letter] != 0:
+	for x in ana_li:
+		if ana_li[x] != 0:
 			return False
 
 	# Will return True if all checks pass
@@ -44,11 +45,7 @@ def anagram_check_sorted(s1, s2):
 	s1 = s1.replace(" ", "").lower()
 	s2 = s2.replace(" ", "").lower()
 
-	# If length isn't the same, then no way to be an anagram
-	if len(s1) != len(s2):
-		return False
-	else:
-		return sorted(s1) == sorted(s2)
+	return sorted(s1) == sorted(s2)
 
 if __name__ == '__main__':
 	s1 = "tram"
